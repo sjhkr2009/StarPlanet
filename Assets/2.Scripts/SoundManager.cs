f294ddbf8048fcb18db8f183b2fcfff642854e80
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-01-17 12:08:37 +0900

    a0.63

---
+++ Assets/2.Scripts/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public enum SoundTypeFX { CorrectCol, WrongCol }
+
+public class SoundManager : MonoBehaviour
+{
+    //[BoxGroup("BGM")] [SerializeField] AudioSource audioSource;
+
+    [BoxGroup("Enemy Destroy")] [SerializeField] AudioClip correctCollision;
+    [BoxGroup("Enemy Destroy")] [SerializeField] AudioClip wrongCollision;
+
+    List<AudioSource> audioFXPlayers = new List<AudioSource>();
+
+    public void MakeAudioList(List<GameObject> gameObjects)
+    {
+        for (int i = 0; i < gameObjects.Count; i++)
+        {
+            audioFXPlayers.Add(gameObjects[i].GetComponent<AudioSource>());
+        }
+    }
+
+    public void PlayFXSound(SoundTypeFX soundType)
+    {
+        AudioSource audio = FindAudioFXPlayer();
+
+        switch (soundType)
+        {
+            case SoundTypeFX.CorrectCol:
+                audio.clip = correctCollision;
+                audio.volume = 0.7f;
+                audio.Play();
+                break;
+            case SoundTypeFX.WrongCol:
+                audio.clip = wrongCollision;
+                audio.volume = 0.4f;
+                audio.Play();
+                break;
+            
+        }
+    }
+
+    AudioSource FindAudioFXPlayer()
+    {
+        AudioSource container = audioFXPlayers[0];
+        for (int i = 0; i < audioFXPlayers.Count; i++)
+        {
+            if (!audioFXPlayers[i].isPlaying)
+            {
+                container = audioFXPlayers[i];
+                break;
+            }
+        }
+        if(!container.gameObject.activeSelf) container.gameObject.SetActive(true);
+        return container;
+    }
+}
