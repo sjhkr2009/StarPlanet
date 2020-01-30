378c0bd8927fa175801ade6651d99dd6423636fd
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-01-30 12:19:24 +0900

    StarPlanet a0.82 Score/Spawn Tier Separation

--- Assets/2.Scripts/Enemy.cs
+++ Assets/2.Scripts/Enemy.cs
@@ -3,21 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
-public enum EnemyType { ToPlanet1, ToStar1 }
+public enum EnemyType { ToPlanet1, ToStar1, ToPlanet2, ToStar2 }
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] EnemyType enemyType;
     public EnemyType EnemyType => enemyType;
 
-    [SerializeField] private string targetType;
-    [SerializeField] private string avoidType;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private int damage;
-    [SerializeField] private int healing;
+    [BoxGroup("Basic"), SerializeField] private string targetType;
+    [BoxGroup("Basic"), SerializeField] private string avoidType;
+    [BoxGroup("Basic"), SerializeField] private int damage;
+    [BoxGroup("Basic"), SerializeField] private int healing;
+
+    [TabGroup("Type 1"), SerializeField] private float moveSpeed;
+
+    [TabGroup("Type 2"), SerializeField] private float radiusReduceSpeed;
+    [TabGroup("Type 2"), SerializeField] private float angulerSpeed;
 
     public event Action<Enemy, int> EventContactCorrect;
     public event Action<Enemy, int> EventContactWrong;
+    public event Action<Enemy> EventOnExplosion;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -29,6 +34,30 @@ public class Enemy : MonoBehaviour
         {
             EventContactWrong(this, damage);
         }
+        else if (other.CompareTag("HexagonExplosion") && Vector2.Distance(transform.position, other.bounds.center) > Vector3.Distance(other.bounds.extents, other.bounds.center) * 0.8f)
+        {
+            EventOnExplosion(this);
+        }
+        else if (other.CompareTag("Explosion"))
+        {
+            EventOnExplosion(this);
+        }
+    }
+
+
+
+    private void OnEnable()
+    {
+        if(targetType != "Star" && targetType != "Planet")
+        {
+            if (enemyType == EnemyType.ToPlanet1 || enemyType == EnemyType.ToPlanet2) targetType = "Planet";
+            else if (enemyType == EnemyType.ToStar1 || enemyType == EnemyType.ToStar2) targetType = "Star";
+        }
+        if (avoidType != "Star" && avoidType != "Planet")
+        {
+            if (enemyType == EnemyType.ToPlanet1 || enemyType == EnemyType.ToPlanet2) avoidType = "Star";
+            else if (enemyType == EnemyType.ToStar1 || enemyType == EnemyType.ToStar2) avoidType = "Planet";
+        }
     }
 
 
@@ -37,9 +66,42 @@ public class Enemy : MonoBehaviour
         transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
     }
 
+    void RotateMove(bool isClockwise)
+    {
+        float currentRadius = Vector3.Distance(transform.position, Vector3.zero);
+        float currentAngle = Mathf.Atan2(transform.position.z, transform.position.x);
+
+        float _targetAngle;
+        if (isClockwise) _targetAngle = currentAngle - angulerSpeed * Mathf.Deg2Rad * Time.deltaTime;
+        else _targetAngle = currentAngle + angulerSpeed * Mathf.Deg2Rad * Time.deltaTime;
+
+        float targetRadius = currentRadius - radiusReduceSpeed * Time.deltaTime;
+
+        float _nextPosX = targetRadius * Mathf.Cos(_targetAngle);
+        float _nextPosY = targetRadius * Mathf.Sin(_targetAngle);
+        Vector3 _nextPos = new Vector3(_nextPosX, transform.position.y, _nextPosY);
+
+        transform.rotation = Quaternion.LookRotation(_nextPos);
+        transform.position = _nextPos;
+    }
+
 
     private void Update()
     {
-        Move();
+        switch (enemyType)
+        {
+            case EnemyType.ToPlanet1:
+                Move();
+                break;
+            case EnemyType.ToStar1:
+                Move();
+                break;
+            case EnemyType.ToPlanet2:
+                RotateMove(true);
+                break;
+            case EnemyType.ToStar2:
+                RotateMove(false);
+                break;
+        }
     }
 }
