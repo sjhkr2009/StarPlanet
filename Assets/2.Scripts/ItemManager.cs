3c4ada8195d98802b47f7bc9c584b647700894c3
sjhkr2009 <49022232+sjhkr2009@users.noreply.github.com>
2020-02-06 19:55:15 +0900

    a0.91 Create HealKit

--- Assets/2.Scripts/ItemManager.cs
+++ Assets/2.Scripts/ItemManager.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
 
 public class ItemManager : MonoBehaviour
 {
+    public event Action<int> EventOnScoreChange = n => { };
+    
     [SerializeField] PoolManager poolManager;
     [SerializeField] SoundManager soundManager;
     [SerializeField] ParticleManager particleManager;
@@ -14,6 +17,8 @@ public class ItemManager : MonoBehaviour
     [TabGroup("Hexagon Bomb"), SerializeField] private float minDelayOfHexagonBomb;
     [TabGroup("Hexagon Bomb"), SerializeField] private float maxDelayOfHexagonBomb;
     [TabGroup("Hexagon Bomb"), SerializeField] Transform planet;
+    [TabGroup("Healkit"), SerializeField] private float minDelayOfHealkit;
+    [TabGroup("Healkit"), SerializeField] private float maxDelayOfHealkit;
 
     float screenView;
 
@@ -27,8 +32,33 @@ public class ItemManager : MonoBehaviour
 
         StartCoroutine(nameof(HexagonBombSpawn));
         StartCoroutine(nameof(FixedBombSpawn));
+        StartCoroutine(nameof(HealkitSpawn));
     }
 
+    void HealingToStar(ItemHeal item, int healValue)
+    {
+        GameManager.Instance.PlayerHPChange(true, healValue);
+        particleManager.SpawnParticle(ParticleType.Healing, item.transform);
+        soundManager.PlayFXSound(SoundTypeFX.Healing);
+
+        DisableHealkit(item);
+    }
+
+    void HealingToPlanet(ItemHeal item, int healValue)
+    {
+        GameManager.Instance.PlayerHPChange(false, healValue);
+        particleManager.SpawnParticle(ParticleType.Healing, item.transform);
+        soundManager.PlayFXSound(SoundTypeFX.Healing);
+
+        DisableHealkit(item);
+    }
+
+    void DisableHealkit(ItemHeal item)
+    {
+        item.EventOnHealingPlanet -= HealingToPlanet;
+        item.EventOnHealingStar -= HealingToStar;
+        item.gameObject.SetActive(false);
+    }
 
     void CreateExplosion(ItemBomb item)
     {
@@ -36,7 +66,7 @@ public class ItemManager : MonoBehaviour
         {
             case ExplosionType.Hexagon:
                 float size = Vector3.Distance(item.transform.position, Vector3.zero);
-                Explosion explosion = (Explosion)particleManager.SpawnParticle(ParticleType.HexagonExplosion, planet);
+                Explosion explosion = (Explosion)particleManager.SpawnAndGetParticle(ParticleType.HexagonExplosion, planet);
                 explosion.transform.localScale = Vector3.one * size;
                 soundManager.PlayFXSound(SoundTypeFX.HexagonBomb);
                 break;
@@ -59,18 +89,18 @@ public class ItemManager : MonoBehaviour
     {
         while (true)
         {
-            float delay = Random.Range(minDelayOfHexagonBomb, maxDelayOfHexagonBomb);
+            float delay = UnityEngine.Random.Range(minDelayOfHexagonBomb, maxDelayOfHexagonBomb);
             yield return new WaitForSeconds(delay);
 
             float cameraSizeX = Camera.main.orthographicSize * screenView;
             float cameraSizeY = Camera.main.orthographicSize;
 
-            Vector3 spawnPosUp = new Vector3(Random.Range(-cameraSizeX, cameraSizeX), 0f, cameraSizeY + 1f);
-            Vector3 spawnPosRight = new Vector3(cameraSizeX + 1f, 0f, Random.Range(-cameraSizeY, cameraSizeY));
-            Vector3 spawnPosDown = new Vector3(Random.Range(-cameraSizeX, cameraSizeX), 0f, -cameraSizeY - 1f);
-            Vector3 spawnPosLeft = new Vector3(-cameraSizeX - 1f, 0f, Random.Range(-cameraSizeY, cameraSizeY));
+            Vector3 spawnPosUp = new Vector3(UnityEngine.Random.Range(-cameraSizeX, cameraSizeX), 0f, cameraSizeY + 1f);
+            Vector3 spawnPosRight = new Vector3(cameraSizeX + 1f, 0f, UnityEngine.Random.Range(-cameraSizeY, cameraSizeY));
+            Vector3 spawnPosDown = new Vector3(UnityEngine.Random.Range(-cameraSizeX, cameraSizeX), 0f, -cameraSizeY - 1f);
+            Vector3 spawnPosLeft = new Vector3(-cameraSizeX - 1f, 0f, UnityEngine.Random.Range(-cameraSizeY, cameraSizeY));
 
-            float getPositionRandom = Random.value;
+            float getPositionRandom = UnityEngine.Random.value;
 
             ItemBomb newObject = null;
 
@@ -98,7 +128,7 @@ public class ItemManager : MonoBehaviour
     {
         while (true)
         {
-            float delay = Random.Range(minDelayOfFixedBomb, maxDelayOfFixedBomb);
+            float delay = UnityEngine.Random.Range(minDelayOfFixedBomb, maxDelayOfFixedBomb);
             yield return new WaitForSeconds(delay);
 
             float cameraSizeX = Camera.main.orthographicSize * screenView;
@@ -108,10 +138,50 @@ public class ItemManager : MonoBehaviour
 
             while (Vector3.Distance(spawnPos, Vector3.zero) < 2.5f)
             {
-                spawnPos = new Vector3(Random.Range(-cameraSizeX, cameraSizeX), 0f, Random.Range(-cameraSizeY, cameraSizeY));
+                spawnPos = new Vector3(UnityEngine.Random.Range(-cameraSizeX * 0.95f, cameraSizeX * 0.95f), 0f, UnityEngine.Random.Range(-cameraSizeY * 0.95f, cameraSizeY * 0.95f));
             }
             ItemBomb newObject = (ItemBomb)poolManager.Spawn(ObjectPool.ItemFixedBomb, spawnPos, Quaternion.identity);
             newObject.EventOnExplosion += OnBombExplosion;
+        }
+    }
+
+    IEnumerator HealkitSpawn()
+    {
+        while (true)
+        {
+            float delay = UnityEngine.Random.Range(minDelayOfHealkit, maxDelayOfHealkit);
+            yield return new WaitForSeconds(delay);
+
+            float cameraSizeX = Camera.main.orthographicSize * screenView;
+            float cameraSizeY = Camera.main.orthographicSize;
+
+            Vector3 spawnPosUp = new Vector3(UnityEngine.Random.Range(-cameraSizeX, cameraSizeX), 0f, cameraSizeY + 1f);
+            Vector3 spawnPosRight = new Vector3(cameraSizeX + 1f, 0f, UnityEngine.Random.Range(-cameraSizeY, cameraSizeY));
+            Vector3 spawnPosDown = new Vector3(UnityEngine.Random.Range(-cameraSizeX, cameraSizeX), 0f, -cameraSizeY - 1f);
+            Vector3 spawnPosLeft = new Vector3(-cameraSizeX - 1f, 0f, UnityEngine.Random.Range(-cameraSizeY, cameraSizeY));
+
+            float getPositionRandom = UnityEngine.Random.value;
+
+            ItemHeal newObject = null;
+
+            if (getPositionRandom < 0.25f)
+            {
+                newObject = (ItemHeal)poolManager.Spawn(ObjectPool.ItemHeal, spawnPosUp, Quaternion.LookRotation(spawnPosDown));
+            }
+            else if (getPositionRandom < 0.5f)
+            {
+                newObject = (ItemHeal)poolManager.Spawn(ObjectPool.ItemHeal, spawnPosRight, Quaternion.LookRotation(spawnPosLeft));
+            }
+            else if (getPositionRandom < 0.75f)
+            {
+                newObject = (ItemHeal)poolManager.Spawn(ObjectPool.ItemHeal, spawnPosDown, Quaternion.LookRotation(spawnPosUp));
+            }
+            else
+            {
+                newObject = (ItemHeal)poolManager.Spawn(ObjectPool.ItemHeal, spawnPosLeft, Quaternion.LookRotation(spawnPosRight));
+            }
+            newObject.EventOnHealingPlanet += HealingToPlanet;
+            newObject.EventOnHealingStar += HealingToStar;
         }
     }
 }
